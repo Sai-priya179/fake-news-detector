91881602d8f16cb3c4d1a353cf77541d98d831ff
Yasaswini Saipriya Gunnam <saipriyaa75@gmail.com>
2025-06-24 10:56:48 +0530

    Add files via upload

---
+++ Fake news detector/train/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model/classifier.pkl', 'rb'))
+vectorizer = pickle.load(open('model/vectorizer.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    news = request.form['news']
+    input_data = vectorizer.transform([news])
+    prediction = model.predict(input_data)[0]
+    label = "REAL" if prediction == 1 else "FAKE"
+    return render_template('index.html', prediction=label, news=news)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
